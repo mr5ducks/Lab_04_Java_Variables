45fbdb3437bd1d8a2ecaed94b5296b6c6e9b42a2
mr5ducks <jojog7d@yahoo.com>
2025-05-05 18:29:20 -0400

    work

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int intOperandA = 1;
+        int intOperandB = 5;
+        int intSum = 2;
+        int intProduct = 7;
+        int intDifference = 4;
+        int intQuotient = 8;
+        int intModulo = 9;
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 2.50;
+        //keep these good for testing
+        double doubleSum = 1.25;
+        double doubleProduct = 3.10;
+        double doubleDifference = 6.15;
+        double doubleQuotient = 9.07;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+        System.out.println(doubleOperandA + intModulo);
+        System.out.println(intSum / doubleOperandB);
+        System.out.println(intOperandA + intOperandB);
+        System.out.println(intSum + intProduct);
+        System.out.println(intDifference + intQuotient);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
